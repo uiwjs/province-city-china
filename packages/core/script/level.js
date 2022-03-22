0fcb5dd6e1dea179e961bd655349403e35ab7e95
jaywcjlove <398188662@qq.com>
2022-03-22 10:06:19 +0800

    chore: update scripts (#35)

---
+++ packages/core/script/level.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const FS = require('fs-extra');
+const city = require('../dist/city.json')
+const area = require('../dist/area.json')
+const province = require('../dist/province.json')
+
+;(async () => {
+  try {
+    // =================================================
+    // --------存储一个体积大的层级数据----------->
+    // =================================================
+    console.log(`✅ >\x1b[35;1m <省市区>\x1b[0m层级数据：`);
+    const dataPath = path.resolve(process.cwd(), 'dist/level.json');
+    const data = province.map(p => {
+      let cData = [...city].filter(m => m.province === p.province && /00$/.test(m.code)).map(m => {
+        m.children = area.filter(a => a.city === m.city && a.province === m.province);
+        return m
+      });
+      if (/^(11|31|12|50)$/.test(p.province) && cData.length === 0) {
+        cData = area.filter(c => c.province === p.province)
+      }
+      p.children = cData;
+      return p;
+    });
+    await FS.outputFile(dataPath, JSON.stringify(data, null, 2));
+    console.log('  \x1b[32;1m✔\x1b[0m 数据保存:', path.relative(process.cwd(), dataPath));
+
+    const minDataPath = path.resolve(process.cwd(), 'dist/level.min.json');
+    const minData = province.map(p => {
+      let cData = [...city].filter(m => m.province === p.province && /00$/.test(m.code)).map(m => {
+        m.children = area.filter(a => a.city === m.city && a.province === m.province).map((child) => ({
+          c: child.code, n: child.name, p: child.province, d: child.city, a: child.area
+        }));
+        return { c: m.code, n: m.name, p: m.province, d: m.children };
+      });
+      if (/^(11|31|12|50)$/.test(p.province) && cData.length === 0) {
+        cData = area.filter(c => c.province === p.province).map((child) => ({
+          c: child.code, n: child.name, p: child.province, d: child.city, a: child.area
+        }));
+      }
+      p.children = cData;
+      return { c: p.code, n: p.name, p: p.province, d: p.children };
+    });
+    await FS.outputFile(minDataPath, JSON.stringify(minData));
+    console.log('  \x1b[32;1m✔\x1b[0m 数据保存:', path.relative(process.cwd(), minDataPath));
+  } catch (error) {
+    console.log(error)
+    console.log(`\x1b[31;1m ERR:验证<层级数据>数据[]\x1b[0m: ${error.massage}`);
+  }
+})();
--- packages/core/script/sql.js
+++ packages/core/script/sql.js
@@ -4,31 +4,31 @@
  */
 const fs = require('fs-extra');
  
- const sql = `# ************************************************************
- # 中华人民共和国行政区划代码 SQL dump
- # Generation Time: ${new Date()}
- # ************************************************************
- 
- DROP TABLE IF EXISTS \`province\`;
- CREATE TABLE \`province\` (
-   \`id\` int(11) unsigned NOT NULL AUTO_INCREMENT,
-   \`code\` bigint(12) DEFAULT NULL COMMENT '行政区划代码',
-   \`name\` varchar(32) DEFAULT NULL COMMENT '名称',
-   \`province\` varchar(32) DEFAULT NULL COMMENT '省/直辖市',
-   \`city\` varchar(32) DEFAULT NULL COMMENT '市',
-   \`area\` varchar(32) DEFAULT NULL COMMENT '区',
-   \`town\` varchar(32) DEFAULT NULL COMMENT '城镇地区',
-   PRIMARY KEY (\`id\`)
- ) ENGINE=InnoDB;
- 
- LOCK TABLES \`province\` WRITE;
- 
- INSERT INTO \`province\` (\`id\`, \`code\`, \`name\`, \`province\`, \`city\`, \`area\`, \`town\`)
- VALUES
- {{sqldata}};
- 
- UNLOCK TABLES;
- `;
+const sql = `# ************************************************************
+# 中华人民共和国行政区划代码 SQL dump
+# Generation Time: ${new Date()}
+# ************************************************************
+
+DROP TABLE IF EXISTS \`province\`;
+CREATE TABLE \`province\` (
+  \`id\` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  \`code\` bigint(12) DEFAULT NULL COMMENT '行政区划代码',
+  \`name\` varchar(32) DEFAULT NULL COMMENT '名称',
+  \`province\` varchar(32) DEFAULT NULL COMMENT '省/直辖市',
+  \`city\` varchar(32) DEFAULT NULL COMMENT '市',
+  \`area\` varchar(32) DEFAULT NULL COMMENT '区',
+  \`town\` varchar(32) DEFAULT NULL COMMENT '城镇地区',
+  PRIMARY KEY (\`id\`)
+) ENGINE=InnoDB;
+
+LOCK TABLES \`province\` WRITE;
+
+INSERT INTO \`province\` (\`id\`, \`code\`, \`name\`, \`province\`, \`city\`, \`area\`, \`town\`)
+VALUES
+{{sqldata}};
+
+UNLOCK TABLES;
+`;
  
  /**
   * 输出 SQL 文件，此为所有数据的输出
@@ -51,14 +51,23 @@ const fs = require('fs-extra');
   const town = require('../dist/town.json');
 
   const data = [...province, ...city, ...area, ...town].map(item => {
-    return { ...item, city: item.code.substring(2,4) || 0, area: item.code.substring(4,6) || 0, town: item.code.substring(6) || 0 }
+    return { ...item, city: item.city || 0, area: item.area || 0, town: item.town || 0 }
   });
   
+  const mindata = [...province, ...city, ...area, ...town].map(item => {
+    return { c: item.code, n: item.name, p: item.province, y: item.city || 0, a: item.area || 0, t: item.town || 0 }
+  });
+  
+  console.log(`✅ >\x1b[35;1m <省市区>\x1b[0m数据：`);
   await fs.outputFile('./dist/data.json', JSON.stringify(data, null, 2));
   console.log('  \x1b[32;1m✔\x1b[0m 保存与 <town.json> 合并的数据: ./dist/data.json');
+  
+  await fs.outputFile('./dist/data.min.json', JSON.stringify(mindata));
+  console.log('  \x1b[32;1m✔\x1b[0m 保存与 <town.json> 合并的数据: ./dist/data.min.json');
 
-   const sqlStr = outPutSQL(data);
-   await fs.outputFile('./dist/data.sql', sqlStr);
-   console.log('  \x1b[32;1m✔\x1b[0m 数据保存: ./dist/data.sql');
+  console.log(`✅ >\x1b[35;1m <SQL>\x1b[0m数据：`);
+  const sqlStr = outPutSQL(data);
+  await fs.outputFile('./dist/data.sql', sqlStr);
+  console.log('  \x1b[32;1m✔\x1b[0m 数据保存: ./dist/data.sql');
 
  })();
