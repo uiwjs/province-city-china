c1caa5b1e18794c8cde9f26479b3c9565335ce60
jaywcjlove <jaywcjlove@users.noreply.github.com>
2023-11-17 21:46:42 +0000

    v8.5.7 released v8.5.7 (#49) 668fcb202e08e06c805900204a14d6afeec9186c

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 # ************************************************************
 # 中华人民共和国行政区划代码 SQL dump
-# Generation Time: Fri Nov 17 2023 21:45:03 GMT+0000 (Coordinated Universal Time)
+# Generation Time: Fri Nov 17 2023 21:46:36 GMT+0000 (Coordinated Universal Time)
 # ************************************************************
 
 DROP TABLE IF EXISTS `province`;
