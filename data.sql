df17c91fdc9f271b34bcc000d7b7c28a311f2165
jaywcjlove <jaywcjlove@users.noreply.github.com>
2025-12-03 16:19:01 +0000

    v8.5.8 doc: Update README.md c1da45b2e231d17063f3699079d40f7eb5a6ff5c

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 # ************************************************************
 # 中华人民共和国行政区划代码 SQL dump
-# Generation Time: Sun Nov 09 2025 06:43:35 GMT+0000 (Coordinated Universal Time)
+# Generation Time: Wed Dec 03 2025 16:18:56 GMT+0000 (Coordinated Universal Time)
 # ************************************************************
 
 DROP TABLE IF EXISTS `province`;
